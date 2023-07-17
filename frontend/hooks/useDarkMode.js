03af1eb3c7e97e23746cea85e6e7bb05af1278c9
Easton <easton.fritsch21@gmail.com>
2023-07-17 09:41:41 -0600

    created useLocalStorage hook and composed it

--- frontend/hooks/useDarkMode.js
+++ frontend/hooks/useDarkMode.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
+import useLocalSotorage from "./useLocalStorage";
 
 const useDarkMode = (initialValue) => {
-    const [darkMode, setDarkMode] = useState(initialValue);
+    const [darkMode, setDarkMode] = useLocalSotorage("darkMode", initialValue);
     return [darkMode, setDarkMode];
 }
 
